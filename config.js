275d57ffba902130ba86ef70f8db2d4cd86085e5
Roberto Viquez <robertoviquez@gmail.com>
2018-07-20 18:31:07 -0600

    First Commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    'auth': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTA5OSwiaWRlbiI6IjQ2NDI0ODkwNzU5Mzk0MDk5MyIsIm1kIjp7fSwidHMiOjE1MzA3NTY4OTk1OTB9.nVSg9amQUzWa4PxKlfS3OqAqjYtoNWCrtcJgTI8dtrs'
+    //'secret': 'ilovescotchyscotch',
+    // 'database': 'mongodb://noder:noderauth&54;proximus.modulusmongo.net:27017/so9pojyN'
+    //'database': 'mongodb://localhost:27017/LocalTests'
+
+};
